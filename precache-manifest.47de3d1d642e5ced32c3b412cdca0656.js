de153b7ba84c6cdf5d6baaf3ef5335ff92d4388c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-15 03:25:27 +0000

    Deploy karnnivore/NodeJs_TravisCi_wed-1 to github.com/karnnivore/NodeJs_TravisCi_wed-1.git:gh-pages

---
+++ precache-manifest.47de3d1d642e5ced32c3b412cdca0656.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "431ba7243578dddcb46e",
+    "url": "/static/css/main.5b665750.chunk.css"
+  },
+  {
+    "revision": "431ba7243578dddcb46e",
+    "url": "/static/js/main.431ba724.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "42ac842b7fdacc3c3ee45294f1500dbd",
+    "url": "/index.html"
+  }
+];
